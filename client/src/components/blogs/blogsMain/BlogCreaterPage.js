364ff19cf48441cf62a3c74b7d47daecad280fad
gorvardanyan1 <gorvardanyan1>
2022-12-31 02:00:31 +0400

    dayEnd

--- client/src/components/blogs/blogsMain/BlogCreaterPage.js
+++ client/src/components/blogs/blogsMain/BlogCreaterPage.js
@@ -2,7 +2,18 @@ import React from 'react'
 import '../../../style/blogCreaterPage.style.scss'
 const BlogCreaterPage = () => {
     return (
-        <div className='blogCreaterPage'>BlogCreaterPage</div>
+        <div className='blogCreaterPage'>
+            <select name="alo" id="alo">
+                <option value="hi">Hi</option>
+                <option value="Alo">Alo</option>
+                <option value="Blo">BLo</option>
+            </select>
+            {/* <iframe height="300" style={{ width: '100%' }} scrolling="no" title="Untitled" src="https://codepen.io/gorvardanyan1/embed/zYLqywR?default-tab=js%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+                See the Pen <a href="https://codepen.io/gorvardanyan1/pen/zYLqywR">
+                    Untitled</a> by Gor (<a href="https://codepen.io/gorvardanyan1">@gorvardanyan1</a>)
+                on <a href="https://codepen.io">CodePen</a>.
+            </iframe> */}
+        </div>
     )
 }
 
